8f07e6b2038e4b5ebc70e53932d87d462609fb8f
CallmeHongmaybe <44527246+CallmeHongmaybe@users.noreply.github.com>
2022-04-15 11:41:48 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = (phase, { defaultConfig }) => {
     return {
         env: {
-            MONGO_URL: ``, 
-            UNSPLASH_KEY: "", 
-            JWT_SECRET_KEY: ''
+            MONGO_URL: `mongodb+srv://Dennis:ineedbass12@cluster0-csbbg.mongodb.net/`, 
+            UNSPLASH_KEY: "-VJshLhodJvCxWLdl82nQ16MU-KDs0mM_b33aWueogo", 
+            JWT_SECRET_KEY: '98aa2b82-2375-4bbf-af3f-0c89d334f923'
         },  
     }
 }
